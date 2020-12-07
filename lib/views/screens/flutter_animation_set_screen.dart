abe767b22bb757de3673d29658e8c540991a8368
linnefromice <linnefromice@gmail.com>
2020-12-07 23:28:39 +0900

    create new screen

---
+++ lib/views/screens/flutter_animation_set_screen.dart
@@ -0,0 +1,26 @@
+import 'package:decorated_icon/decorated_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+
+class FlutterAnimationSetScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<FlutterAnimationSetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
