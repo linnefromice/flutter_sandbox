495801087787c724d5f4604da715019882a99e60
linnefromice <linnefromice@gmail.com>
2020-12-07 22:11:34 +0900

    create new screen

---
+++ lib/views/screens/decorated_icon_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DecoratedIconScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<DecoratedIconScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("DecoratedIconScreen"),
+    );
+  }
+}
