384302adbafc0cdce3ea451d1d9196793ce837c3
linnefromice <linnefromice@gmail.com>
2020-12-07 23:15:38 +0900

    use flutter_spinkit

--- lib/views/screens/flutter_spinkit_screen.dart
+++ lib/views/screens/flutter_spinkit_screen.dart
@@ -1,5 +1,6 @@
 import 'package:decorated_icon/decorated_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:like_button/like_button.dart';
 
 class FlutterSpinkitScreen extends StatefulWidget {
@@ -17,7 +18,45 @@ class _State extends State<FlutterSpinkitScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            
+            SpinKitPumpingHeart(
+              itemBuilder: (BuildContext context, int index) {
+                return Icon(
+                  Icons.favorite,
+                  size: 50,
+                  color: Colors.pink,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            SpinKitCircle(
+              itemBuilder: (BuildContext context, int index) {
+                return Icon(
+                  Icons.favorite,
+                  size: 50,
+                  color: Colors.pink,
+                );
+              }
+            ),
+            SizedBox(height: 20),
+            SpinKitRipple(
+              itemBuilder: (BuildContext context, int index) {
+                return Icon(
+                  Icons.favorite,
+                  size: 50,
+                  color: Colors.pink,
+                );
+              }
+            ),
+            SizedBox(height: 20),
+            SpinKitRotatingPlain(
+              itemBuilder: (BuildContext context, int index) {
+                return Icon(
+                  Icons.favorite,
+                  size: 50,
+                  color: Colors.pink,
+                );
+              }
+            )
           ],
         ),
       ),
