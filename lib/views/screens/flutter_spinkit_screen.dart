d8354fd0d33e736fb1f5d0ad5e5f8c0888a8aa02
linnefromice <linnefromice@gmail.com>
2020-12-07 23:08:47 +0900

    update route setting

---
+++ lib/views/screens/flutter_spinkit_screen.dart
@@ -0,0 +1,26 @@
+import 'package:decorated_icon/decorated_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+
+class FlutterSpinkitScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<FlutterSpinkitScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
