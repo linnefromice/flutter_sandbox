8547edcf732188993c5337c93aef345f75e74c46
linnefromice <linnefromice@gmail.com>
2020-12-07 22:23:26 +0900

    use decorated_icon

--- lib/views/screens/decorated_icon_screen.dart
+++ lib/views/screens/decorated_icon_screen.dart
@@ -1,3 +1,4 @@
+import 'package:decorated_icon/decorated_icon.dart';
 import 'package:flutter/material.dart';
 
 class DecoratedIconScreen extends StatefulWidget {
@@ -9,7 +10,57 @@ class _State extends State<DecoratedIconScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("DecoratedIconScreen"),
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            DecoratedIcon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 50,
+              shadows: [
+                BoxShadow(
+                  blurRadius: 12.0,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            DecoratedIcon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 50,
+              shadows: [
+                BoxShadow(
+                  blurRadius: 6.0,
+                  color: Colors.pink[300],
+                ),
+                BoxShadow(
+                  blurRadius: 6.0,
+                  color: Colors.pink[200],
+                  offset: Offset(3.0, 3.0)
+                ),
+              ],
+            ),
+            DecoratedIcon(
+              Icons.android,
+              color: Colors.purple,
+              size: 60.0,
+              shadows: [
+                BoxShadow(
+                  blurRadius: 42.0,
+                  color: Colors.purpleAccent,
+                ),
+                BoxShadow(
+                  blurRadius: 12.0,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
