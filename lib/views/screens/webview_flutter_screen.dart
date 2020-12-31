3084f444dadf878ba4272b74a9b59a8bc7d095c3
linnefromice <linnefromice@gmail.com>
2021-01-01 02:16:25 +0900

    create new screen

---
+++ lib/views/screens/webview_flutter_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebviewFlutterScreen extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<WebviewFlutterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("WebviewFlutterScreen"),
+      ),
+    );
+  }
+}
