d28a4ab3f059003a9f30d9dc61490c346d765ed0
linnefromice <linnefromice@gmail.com>
2021-02-20 22:36:47 +0900

    update ui

--- lib/views/screens/webfeed_screen.dart
+++ lib/views/screens/webfeed_screen.dart
@@ -19,8 +19,24 @@ class WebfeedScreen extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Center(
-        child: Text("WebfeedScreen"),
+      body: FutureBuilder(
+        future: RssService().getFeed(),
+        builder: (BuildContext context, AsyncSnapshot<RssFeed> snapshot) {
+          if (snapshot.hasError) return Center(child: Text(snapshot.error.toString()));
+          if (snapshot.hasData) {
+            final List<RssItem> items = snapshot.data.items;
+            return ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index].title),
+                  subtitle: Text(items[index].link),
+                );
+              },
+            );
+          }
+          return CircularProgressIndicator();
+        },
       ),
     );
   }
