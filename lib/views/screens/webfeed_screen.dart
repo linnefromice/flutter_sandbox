b51a13b1a9f39e94c316268211918bd7f7849445
linnefromice <linnefromice@gmail.com>
2021-02-20 18:38:21 +0900

    create screen

---
+++ lib/views/screens/webfeed_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WebfeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: Center(
+        child: Text("WebfeedScreen"),
+      ),
+    );
+  }
+}
