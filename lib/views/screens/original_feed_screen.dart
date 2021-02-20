55aed2d13f04c0e82da0b759b71c8258a4d23748
linnefromice <linnefromice@gmail.com>
2021-02-21 00:00:24 +0900

    create rss feed logic

--- lib/views/screens/original_feed_screen.dart
+++ lib/views/screens/original_feed_screen.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:xml2json/xml2json.dart';
+import 'package:http/http.dart' as http;
+
+class FeedService {
+  final _targetUrl = 'https://zenn.dev/topics/flutter/feed';
+
+  Future<List> feed() async {
+    final client = new http.Client();
+    final transformer = Xml2Json();
+    return await client.get(_targetUrl).then((response) {
+      return utf8.decode(response.bodyBytes);
+    }).then((bodyString) {
+      transformer.parse(bodyString);
+      final json = transformer.toGData();
+      return jsonDecode(json)['rss']['channel']['item'];
+    });
+  }
+}
 
 class OriginalFeedScreen extends StatelessWidget {
   @override
@@ -10,7 +29,6 @@ class OriginalFeedScreen extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
         ),
       ),
-      body: Center(child: Text("OriginalFeedScreen")),
     );
   }
 }
