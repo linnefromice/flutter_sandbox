038c89e94f6c049474076ef0992d858a4f21bc17
linnefromice <linnefromice@gmail.com>
2021-03-01 23:17:41 +0900

    update example

--- lib/views/screens/bicolor_icon_screen.dart
+++ lib/views/screens/bicolor_icon_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:linnefromice/views/components/bicolor_icon.dart';
 
@@ -7,6 +9,8 @@ class BicolorIconScreen extends StatefulWidget {
 }
 
 class _State extends State<BicolorIconScreen> {
+  double _currentSliderValue = 50;
+
   Widget _buildFavorites() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -40,6 +44,35 @@ class _State extends State<BicolorIconScreen> {
     );
   }
 
+  Widget _buildRatedFavorites() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: min(1, max(0, (_currentSliderValue - 0) / 20)), beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: min(1, max(0, (_currentSliderValue - 20) / 20)), beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: min(1, max(0, (_currentSliderValue - 40) / 20)), beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: min(1, max(0, (_currentSliderValue - 60) / 20)), beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: min(1, max(0, (_currentSliderValue - 80) / 20)), beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+      ],
+    );
+  }
+
+
+  Widget _buildSlider() {
+    return Slider(
+      value: _currentSliderValue,
+      min: 0,
+      max: 100,
+      divisions: 100,
+      label: _currentSliderValue.round().toString(),
+      onChanged: (double value) {
+        setState(() {
+          _currentSliderValue = value;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,6 +90,14 @@ class _State extends State<BicolorIconScreen> {
           Container(
             margin: EdgeInsets.symmetric(vertical: 5),
             child: _buildCreditCards()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildRatedFavorites()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildSlider()
           ),
         ],
       )
