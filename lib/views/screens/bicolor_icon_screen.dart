0a18ac945d7d117aced15ac3eb5496e912d56e02
linnefromice <linnefromice@gmail.com>
2021-03-01 12:53:08 +0900

    update examples

--- lib/views/screens/bicolor_icon_screen.dart
+++ lib/views/screens/bicolor_icon_screen.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:linnefromice/views/components/bicolor_icon.dart';
 
 class BicolorIconScreen extends StatelessWidget {
+  Widget _buildFavorites() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: 0.3, beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: 0.5, beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+        BicolorIcon(iconData: Icons.favorite, iconSize: 50, rate: 0.7, beginAlignment: Alignment.centerLeft, beginColor: Colors.pink, endColor: Colors.white),
+      ],
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,15 +21,8 @@ class BicolorIconScreen extends StatelessWidget {
         children: [
           Container(
             margin: EdgeInsets.symmetric(vertical: 5),
-            child: BicolorIcon(
-              iconData: Icons.favorite,
-              iconSize: 50,
-              rate: 0.25,
-              beginAlignment: Alignment.topCenter,
-              beginColor: Colors.red,
-              endColor: Colors.blue,
-            )
-          )
+            child: _buildFavorites()
+          ),
         ],
       )
     );
