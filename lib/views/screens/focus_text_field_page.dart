cbaf7733cc60e0231c419fef81960210dc5741a9
linnefromice <linnefromice@gmail.com>
2021-06-25 17:30:03 +0900

    use FocusNode from tutorial
    ref: https://flutter.dev/docs/cookbook/forms/focus

--- lib/views/screens/focus_text_field_page.dart
+++ lib/views/screens/focus_text_field_page.dart
@@ -6,11 +6,33 @@ class FocusTextFieldPage extends StatefulWidget {
 }
 
 class _State extends State<FocusTextFieldPage> {
+  FocusNode focusNode;
+
   @override
-  Widget build(BuildContext contesxt) {
+  void initState() {
+    super.initState();
+    focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("FocusTextFieldPage"),
+      ),
+      body: Center(
+        child: TextField(
+          focusNode: focusNode,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => focusNode.requestFocus(),
       ),
     );
   }
